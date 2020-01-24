2521bd8edfbad79c558d9115a6beff91b02c28df
yjw5344 <yjw5344@hanmail.net>
2020-01-25 00:23:02 +0900

    RandomProject & NewwordSearch Init

---
+++ NewWordSearch/NewWordSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewWordSearch
+//
+//  Created by Jungwon on 2020/01/24.
+//  Copyright © 2020 Jungwon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
